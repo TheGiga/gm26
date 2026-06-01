5a49c8cfcdbdd70269b65fd1e3a5e5ed43f8ce6a
TheGiga <49173264+TheGiga@users.noreply.github.com>
2026-06-02 02:49:25 +0300

    init

---
+++ kubejs/client_scripts/main.js
@@ -0,0 +1,7 @@
+ClientEvents.lang('en_us', event => {
+    // Industrial Foregoing
+    event.renameItem('industrialforegoing:machine_frame_pity', 'Basic Plastic Machine Frame')
+    event.renameItem('industrialforegoing:machine_frame_simple', 'Simple Plastic Machine Frame')
+    event.renameItem('industrialforegoing:machine_frame_advanced', 'Advanced Plastic Machine Frame')
+    event.renameItem('industrialforegoing:machine_frame_supreme', 'Supreme Plastic Machine Frame')
+})
